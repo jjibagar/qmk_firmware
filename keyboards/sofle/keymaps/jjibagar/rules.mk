b2bde869faaa964dae602f98c12c3a0001b80eb1
levo <Jose Jorge Ibaez Garcia23029594Q4163aB>
2024-07-25 20:15:35 +0200

    actualizacion

--- keyboards/sofle/keymaps/jjibagar/rules.mk
+++ keyboards/sofle/keymaps/jjibagar/rules.mk
@@ -12,6 +12,7 @@ CONVERT_TO=promicro_rp2040
 BOOTLOADER = rp2040
 
 
-#de cosecha propia
-RGB_MATRIX_ENABLE = yes
+#nuevo para intentar arreglar el error de RGB_MATRIX
+RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = IS31FL3731
+# RGB_MATRIX_ENABLE = IS31FL3731
